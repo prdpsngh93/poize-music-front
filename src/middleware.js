053d0eb9ef5ade94ba71441c0007229e23518209
navjot kaur <navu77196@gmail.com>
2025-07-20 22:20:47 +0530

    changes

--- src/middleware.js
+++ src/middleware.js
@@ -6,13 +6,13 @@ export async function middleware(request) {
   const cookieStore =  await cookies();
   const token = cookieStore.get('token');
 
-  // if (request.nextUrl.pathname.startsWith('/login') && token) {
-  //   return NextResponse.redirect(new URL('/musician-profile', request.url));  
-  // }
+  if (request.nextUrl.pathname.startsWith('/login') && token) {
+    return NextResponse.redirect(new URL('/musician-profile', request.url));  
+  }
 
-  // if (!token && request.nextUrl.pathname.startsWith('/musician-profile')) {
-  //   return NextResponse.redirect(new URL('/login', request.url));  
-  // }
+  if (!token && request.nextUrl.pathname.startsWith('/musician-profile')) {
+    return NextResponse.redirect(new URL('/login', request.url));  
+  }
   
   return NextResponse.next();  
 }
